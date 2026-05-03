9bd05002970cb92a2b4aa48d7688da6f25d8d13b
Amelia <amelia@darkfactory.local>
2026-05-03 05:37:06 +0000

    Add CI Status component - story-002

---
+++ dashboard/src/CiStatus.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Box, Text } from '@chakra-ui/react';
+
+export default function CiStatus() {
+  return (
+    <Box>
+      <Text fontSize="2xl">CI Status: OK</Text>
+    </Box>
+  );
+}
--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { ChakraProvider, Box, Flex, Text } from '@chakra-ui/react';
+import CiStatus from './CiStatus';
 
 function Dashboard() {
   return <Box><Text fontSize="2xl">Dashboard</Text></Box>;
@@ -21,12 +22,14 @@ function App() {
           <Link to="/">Dashboard</Link>
           <Link to="/board">Agile Board</Link>
           <Link to="/settings">Settings</Link>
+          <Link to="/ci">CI Status</Link>
         </Flex>
         <Box p={4}>
           <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/board" element={<AgileBoard />} />
             <Route path="/settings" element={<Settings />} />
+            <Route path="/ci" element={<CiStatus />} />
           </Routes>
         </Box>
       </BrowserRouter>
